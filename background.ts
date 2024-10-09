30073e9bcb7872b72dcd4d73dc2253b7f76c52ab
Brian Tang <ashrielbrian@gmail.com>
2024-10-09 23:52:52 +0800

    Big refactoring: moves all pglite db instance refs from content script to background

--- background.ts
+++ background.ts
@@ -2,8 +2,42 @@ export { }
 
 import { pipeline, env, type PipelineType } from "@xenova/transformers";
 
+import { getDB, search, type SearchResult } from "~db";
+import { PGliteWorker } from 'dist/electric-sql/worker/index.js'
+
 // IMPORTANT: see this issue https://github.com/microsoft/onnxruntime/issues/14445#issuecomment-1625861446
 env.backends.onnx.wasm.numThreads = 1;
+
+const getUrlId = async (worker: PGliteWorker, url: string) => {
+    let res = await worker.query(`SELECT id FROM page WHERE url = $1`, [url]);
+    console.log("URL ID: ", res.rows[0]);
+    return res.rows.length > 0 ? res.rows[0].id : null
+}
+
+const storeEmbeddings = async (worker: PGliteWorker, urlId: string, chunk: string, embedding: number[]) => {
+
+    let embStr = `'[${embedding}]'`
+    const insertRes = await worker.query(`INSERT INTO embedding (page_id, content, embedding) VALUES ($1, $2, ${embStr});`,
+        [urlId, chunk]
+    );
+
+    console.log("Inserted embedding", insertRes)
+}
+
+const urlIsPresentOrInDatetimeRange = async (worker: PGliteWorker, url: string, withinDays: number = 3) => {
+    // fetch from db whether url exists and/or is within the required days
+    let res = await worker.query("SELECT id, createdAt FROM page WHERE url = $1", [url])
+
+    if (res.rows.length > 0) {
+        console.log(`url exist ${url} of rows: ${res.rows}`)
+        return true;
+
+    } else {
+        let out = await worker.query("INSERT INTO page (url, title) VALUES ($1, $2)", [url, "test"]);
+        console.log("Inserted into pages", out.affectedRows)
+    }
+    return false;
+}
 
 class PipelineSingleton {
     // TODO: is it possible to ensure there's only a single instance of transformers Pipeline, and each worker reuses the same Pipeline?
@@ -23,36 +57,70 @@ class PipelineSingleton {
     }
 }
 
+const getLLMPipeline = async () => {
+    return await PipelineSingleton.getInstance((x) => {
+        console.log("Progress update", x)
+    });
+}
+
+const runInference = async (pipeline, chunk: string) => {
+    const output = await pipeline(chunk, {
+        pooling: "mean",
+        normalize: true,
+    });
+
+    const embedding = Array.from(output.data) as number[];
+
+    return embedding;
+}
+
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    console.log("background received:", message)
-    if (message.type === "get_html_embedding") {
 
-        const pipeline = await PipelineSingleton.getInstance((x) => {
-            console.log("Progress update", x)
-        });
+    const pg = await getDB();
+    console.log(`Message is of type: ${message.type}`);
 
-        const sourceUrl = sender.url;
-        const chunk = message.payload;
 
-        console.log("from bg:", sourceUrl, chunk)
+    // check the URL - if it exists, check if age of the URL. if older than a day, delete the old entries, chunk new and get new embs
+    if (message.type === "process_page" && pg) {
 
-        // generate embeddings for each chunk
-        const output = await pipeline(chunk, {
-            pooling: "mean",
-            normalize: true,
-        });
+        const { type, url, textChunks } = message;
 
-        // Extract the embedding output
-        const embedding = Array.from(output.data);
+        if (await urlIsPresentOrInDatetimeRange(pg, url)) {
+            // URL exists OR is still valid. Do not process, and return.
+            sendResponse({ ok: true, error: null, msg: `URL ${url} has been processed before. Skipping...` })
+        } else {
+            const urlId = await getUrlId(pg, url);
+            const pipeline = await getLLMPipeline();
 
+            console.log(`Background received type '${type}', at ${url} with ${textChunks.length} chunks.`);
+
+            if (textChunks.length <= 0) {
+                sendResponse({ ok: false, error: null, msg: `No chunks sent.` })
+                return;
+            }
+
+            console.log(`Here is a sample chunks: ${textChunks[0]}`);
+            for (let chunk of textChunks) {
+
+                let embedding = await runInference(pipeline, chunk);
+                console.log("Generated embedding: ", embedding.length)
+
+                await storeEmbeddings(pg, urlId, chunk, embedding);
+            }
+            sendResponse({ ok: true, error: null, msg: `Done processing ${textChunks.length} chunks.` })
+        }
+
+    } else if (message.type === "get_embedding") {
+        const { chunk } = message;
+        const pipeline = await getLLMPipeline();
+
+        let embedding = await runInference(pipeline, chunk);
         console.log("Generated embedding: ", embedding.length)
 
-        // return the embedding vector to the main thread
-        sendResponse({ ok: true, error: null, embedding, chunk })
-
-        // Keep the message channel open for async response
-        return true;
+        sendResponse({ ok: true, error: null, embedding })
     }
 
+    // Keep the message channel open for async response
+    return true;
 })
 
--- content.tsx
+++ content.tsx
@@ -1,140 +1,8 @@
 import React, { useEffect, useState } from "react"
-import { PGliteWorker } from 'dist/electric-sql/worker/index.js'
-import { vector } from "~dist/electric-sql/vector";
-
 
 const extractHtmlBody = () => {
     return document.body.outerHTML;
 }
-
-const urlIsPresentOrInDatetimeRange = async (worker: PGliteWorker, url: string, withinDays: number = 3) => {
-    // fetch from db whether url exists and/or is within the required days
-    let res = await worker.query("SELECT id, createdAt FROM page WHERE url = $1", [url])
-
-    if (res.rows.length > 0) {
-        console.log(`url exist ${url} of rows: ${res.rows}`)
-        return true;
-
-    } else {
-        let out = await worker.query("INSERT INTO page (url, title) VALUES ($1, $2)", [url, "test"]);
-        console.log("Inserted into pages", out.affectedRows)
-    }
-    return false;
-}
-
-const PlasmoOverlay = () => {
-
-    const [worker, setWorker] = useState<PGliteWorker>(null);
-
-    useEffect(() => {
-        const initWorker = async () => {
-            const newWorker = new PGliteWorker(
-                new Worker(new URL("./worker.js", import.meta.url), {
-                    type: "module"
-                }), {
-                extensions: { vector }
-            });
-
-            console.log("worker waiting to be ready")
-            await newWorker.waitReady;
-            setWorker(newWorker);  // Set the worker in state
-            console.log("worker:", worker)
-
-            // CREATE TABLE IF NOT EXISTS todo (
-            //     id SERIAL PRIMARY KEY,
-            //     task TEXT,
-            //     done BOOLEAN DEFAULT false
-            // );
-            // INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
-            // INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
-            // INSERT INTO todo (task, done) VALUES ('Create a table', true);
-            // INSERT INTO todo (task) VALUES ('Update a task');
-
-            // Database operations
-            await newWorker.exec(`
-                CREATE EXTENSION IF NOT EXISTS vector;
-
-                CREATE TABLE IF NOT EXISTS page(
-                    id SERIAL PRIMARY KEY,
-                    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                    updatedAt TIMESTAMP DEFAULT NOW(),
-                    url TEXT NOT NULL UNIQUE,
-                    title TEXT
-                    );
-                    
-                    CREATE TABLE IF NOT EXISTS embedding(
-                        id SERIAL PRIMARY KEY,
-                    page_id INT REFERENCES page(id) ON DELETE CASCADE,
-                    content TEXT NOT NULL,
-                    embedding vector(384),
-                    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                );
-        `);
-
-            // const result = await newWorker.query(`
-            //     SELECT * FROM todo WHERE id = 1;
-            // `);
-            // console.log(result.rows);
-        }
-
-        initWorker().catch(console.error)
-    }, [])
-
-
-
-    useEffect(() => {
-        if (worker) {
-            const webpageBodyContent = extractHtmlBody();
-            console.log("worker from within second useeffect", worker)
-
-
-            const processPage = async () => {
-                // check the URL - if it exists, check if age of the URL. if older than a day, delete the old entries, chunk new and get new embs
-                if (!await urlIsPresentOrInDatetimeRange(worker, location.href)) {
-                    // if (true) {
-
-                    // grab only the headers and paragraphs from the webpage
-                    const { headers, paragraphs } = extractHeadersAndParagraphs(webpageBodyContent);
-                    const urlId = await getUrlId(worker, location.href);
-
-                    for (let textChunk of [...headers, ...paragraphs]) {
-                        // send the web contents to the background worker; bg worker has the tranformers pipeline loaded
-                        // chrome.runtime.sendMessage({ type: "get_html_embedding", payload: textChunk }, async (backgroundResponse) => {
-                        //     console.log("Response from the worker", backgroundResponse);
-
-                        //     // get and parse the embedding vec, passing it to the pglite worker instance to be stored in the db
-                        //     await storeEmbeddings(worker, location.href, backgroundResponse.chunk, backgroundResponse.embedding);
-
-                        //     return true;
-                        // })
-
-                        let backgroundResponse = await sendTextChunkToBackground(textChunk);
-                        await storeEmbeddings(worker, urlId, backgroundResponse.chunk, backgroundResponse.embedding);
-                    }
-                }
-            }
-            processPage().catch(console.error)
-
-        }
-    }, [worker])
-
-    return null;
-}
-
-export const sendTextChunkToBackground = async (textChunk: string) => {
-    return await chrome.runtime.sendMessage({ type: "get_html_embedding", payload: textChunk })
-}
-const zip = <T, U>(a: T[], b: U[]): [number, T, U][] => a.map((ele, idx) => [idx, ele, b[idx]])
-// const sendTextChunkToBackgroundProm = (textChunk: string) => {
-//     return new Promise((resolve, reject) => {
-//         chrome.runtime.sendMessage({ type: "get_html_embedding", payload: textChunk }, (response) => {
-//             if (chrome.runtime.lastError) {
-//                 return reject(chrome.runtime.lastError);
-//             }
-//             resolve(response)
-//         })
-//     })
-// }
 
 const extractHeadersAndParagraphs = (htmlContent: string) => {
     const parser = new DOMParser();
@@ -153,20 +21,90 @@ const extractHeadersAndParagraphs = (htmlContent: string) => {
     };
 }
 
-const getUrlId = async (worker: PGliteWorker, url: string) => {
-    let res = await worker.query(`SELECT id FROM page WHERE url = $1`, [url]);
-    console.log("URL ID: ", res.rows[0]);
-    return res.rows.length > 0 ? res.rows[0].id : null
+const PlasmoOverlay = () => {
+
+    // const [worker, setWorker] = useState<PGliteWorker>(null);
+
+    // useEffect(() => {
+    //     const initWorker = async () => {
+    //         const newWorker = new PGliteWorker(
+    //             new Worker(new URL("./worker.js", import.meta.url), {
+    //                 type: "module"
+    //             }), {
+    //             extensions: { vector }
+    //         });
+
+    //         console.log("worker waiting to be ready")
+    //         await newWorker.waitReady;
+    //         setWorker(newWorker);  // Set the worker in state
+    //         console.log("worker:", worker)
+
+    //         // CREATE TABLE IF NOT EXISTS todo (
+    //         //     id SERIAL PRIMARY KEY,
+    //         //     task TEXT,
+    //         //     done BOOLEAN DEFAULT false
+    //         // );
+    //         // INSERT INTO todo (task, done) VALUES ('Install PGlite from NPM', true);
+    //         // INSERT INTO todo (task, done) VALUES ('Load PGlite', true);
+    //         // INSERT INTO todo (task, done) VALUES ('Create a table', true);
+    //         // INSERT INTO todo (task) VALUES ('Update a task');
+
+    //         // Database operations
+    //         await newWorker.exec(`
+    //             CREATE EXTENSION IF NOT EXISTS vector;
+
+    //             CREATE TABLE IF NOT EXISTS page(
+    //                 id SERIAL PRIMARY KEY,
+    //                 createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    //                 updatedAt TIMESTAMP DEFAULT NOW(),
+    //                 url TEXT NOT NULL UNIQUE,
+    //                 title TEXT
+    //                 );
+
+    //                 CREATE TABLE IF NOT EXISTS embedding(
+    //                     id SERIAL PRIMARY KEY,
+    //                 page_id INT REFERENCES page(id) ON DELETE CASCADE,
+    //                 content TEXT NOT NULL,
+    //                 embedding vector(384),
+    //                 createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    //             );
+    //     `);
+
+    //         // const result = await newWorker.query(`
+    //         //     SELECT * FROM todo WHERE id = 1;
+    //         // `);
+    //         // console.log(result.rows);
+    //     }
+
+    //     initWorker().catch(console.error)
+    // }, [])
+
+
+
+    useEffect(() => {
+
+        // grab only the headers and paragraphs from the webpage
+        const webpageBodyContent = extractHtmlBody();
+        const { headers, paragraphs } = extractHeadersAndParagraphs(webpageBodyContent);
+
+        const processPage = async () => {
+
+            const backgroundResponse = await processPageInBackground(location.href, [...headers, ...paragraphs])
+
+            console.log("Page has been processed. Background response: ", backgroundResponse)
+        }
+        processPage().catch(console.error)
+
+    }, [])
+
+    return null;
 }
 
-const storeEmbeddings = async (worker: PGliteWorker, urlId: string, chunk: string, embedding: number[]) => {
-
-    let embStr = `'[${embedding}]'`
-    const insertRes = await worker.query(`INSERT INTO embedding (page_id, content, embedding) VALUES ($1, $2, ${embStr});`,
-        [urlId, chunk]
-    );
-
-    console.log("Inserted embedding", insertRes)
+export const processPageInBackground = async (url: string, webHeadersAndParas: string[]) => {
+    return await chrome.runtime.sendMessage({ type: "process_page", url, textChunks: webHeadersAndParas })
 }
+
+const zip = <T, U>(a: T[], b: U[]): [number, T, U][] => a.map((ele, idx) => [idx, ele, b[idx]])
+
 
 export default PlasmoOverlay
