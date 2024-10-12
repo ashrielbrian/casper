eeb7f9ba62ed1ed1d48e7a8ba62e57d7290deb4b
Brian Tang <ashrielbrian@gmail.com>
2024-10-12 15:19:21 +0800

    Stylizes header and search bar

--- popup.tsx
+++ popup.tsx
@@ -5,6 +5,8 @@ import { vector } from "~dist/electric-sql/vector";
 import { PGliteWorker } from 'dist/electric-sql/worker/index.js'
 
 import "./style.css"
+import { Input } from "~components/Input";
+import { Button } from "~components/Button";
 
 const sendTextChunkToBackground = async (chunk: string) => {
     return await chrome.runtime.sendMessage({ type: "get_embedding", chunk })
@@ -45,22 +47,22 @@ function IndexPopup() {
 
 
     return (
-        <div className="p-16">
-            {/* style={{
-                padding: 16
-            }} */}
-            <h2>
-                Welcome to your{" "}
-                <a href="https://www.plasmo.com" target="_blank">
-                    Plasmo
-                </a>{" "}
-                Extension!
-            </h2>
+        <div className="p-8 w-[36rem] h-96 flex flex-col space-y-2 bg-cyan-50">
+            <div className="space-y-1">
+                {/* Header section */}
+                <h4 className="font-extrabold text-xl">
+                    👻 Casper
+                </h4>
+                <p className="font-mono text-slate-500 text-xs">Close your tabs freely - search them later.</p>
+            </div>
 
-            {/* Input and Search button to provide text to search */}
-            <input type="text" name="search" id="search" value={textToSearch} onChange={(e) => setTextToSearch(e.target.value)} placeholder="Enter text to search" />
-            <button onClick={searchPastPages} disabled={!textToSearch}>Search</button>
 
+            {/* This child div is positioned relative to the parent, with negative margin to make it span full-width, effectively negating its parent's padding */}
+            <div className="relative -mx-8 px-8 py-4 space-x-2 flex items-center">
+                {/* Input and Search button to provide text to search */}
+                <Input type="text" className="flex-grow" value={textToSearch} onChange={(e) => setTextToSearch(e.target.value)} placeholder="Remind me what websites I've visited.." />
+                <Button onClick={searchPastPages} disabled={!textToSearch}> Search</Button>
+            </div>
 
             <ul>
                 {data.length > 0 ? (
@@ -72,8 +74,8 @@ function IndexPopup() {
                         </li>
                     ))
                 ) : (<></>)}
-
             </ul>
+
             {/* <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a> */}
