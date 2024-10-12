061294eea3f760d2fc2fb3cae5492089cd680805
Brian Tang <ashrielbrian@gmail.com>
2024-10-12 16:44:11 +0800

    Adds search results table

---
+++ components/SearchResults.tsx
@@ -0,0 +1,46 @@
+import { Table, TableCaption, TableHeader, TableRow, TableHead, TableCell, TableBody } from "./Table"
+import { type SearchResult } from "~db"
+
+export const SearchResultsTable = ({ results }: { results: SearchResult[] }) => {
+    const extractDomain = (url: string) => {
+        // Create a URL object to parse the URL
+        const parsedUrl = new URL(url);
+
+        // Extract the hostname from the parsed URL
+        let domain = parsedUrl.hostname;
+
+        // Remove 'www.' if present
+        if (domain.startsWith('www.')) {
+            domain = domain.slice(4);
+        }
+
+        return domain;
+    }
+
+
+    return (
+        <Table>
+            <TableCaption>Websites you've visited</TableCaption>
+            <TableHeader className="font-light">
+                <TableRow>
+                    <TableHead className="w-[100px]">Link</TableHead>
+                    <TableHead>Content</TableHead>
+                    <TableHead>Similarity</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {results.map((res) => (
+                    <TableRow key={res.id}>
+                        <TableCell className="font-light">
+                            <a href={res.url} className="relative after:content-[''] after:absolute after:w-0 after:h-[1px] after:bg-current after:bottom-0 after:left-0 hover:after:w-full after:transition-width after:duration-300 after:ease-out">
+                                {extractDomain(res.url)}
+                            </a>
+                        </TableCell>
+                        <TableCell className="text-sm">{res.content}</TableCell>
+                        <TableCell>{res.prob.toFixed(2)}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
