4a92d8428c0add25cb3f646eded58c3ce97ed03e
Brian Tang <ashrielbrian@gmail.com>
2024-10-15 23:46:23 +0800

    Adds dialog for delete db confirmation

---
+++ components/DeleteDialogButton.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+import { Button } from "~/components/Button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "~/components/Dialog"
+
+
+interface DeleteConfirmationDialogProps {
+    onConfirm: () => void;
+}
+
+export function DeleteDialogButton({ onConfirm }: DeleteConfirmationDialogProps) {
+
+    const [open, setOpen] = useState(false);
+
+    const handleConfirm = () => {
+        onConfirm();
+        setOpen(false);
+    };
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button variant="destructive">Clear Database</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>Confirm Deletion</DialogTitle>
+                    <DialogDescription>
+                        All your past embeddings and visited sites will be deleted. This action cannot be undone. Are you sure?
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="sm:justify-start">
+                    <Button onClick={handleConfirm} variant="destructive">
+                        Yes, byebye database
+                    </Button>
+                    <DialogClose asChild>
+                        <Button variant="secondary">
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
