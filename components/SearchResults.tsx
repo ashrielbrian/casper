24f1b8eb737a07044146efa761384f064e330195
Brian Tang <ashrielbrian@gmail.com>
2024-10-19 23:59:09 +0800

    Table cosmetics

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -11,21 +11,20 @@ export const SearchResultsTable = ({ results }: { results: SearchResult[] }) => 
                 <TableRow>
                     <TableHead className="w-[100px]">Link</TableHead>
                     <TableHead>Content</TableHead>
-                    <TableHead>Similarity</TableHead>
+                    <TableHead>Score</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
                 {results.map((res) => (
                     <TableRow key={res.id}>
                         <TableCell className="font-bold">
-                            <a href={res.url} className="link-underline-animation">
+                            <a href={`${res.url}#${res.chunk_tag_id}`} className="link-underline-animation">
                                 {extractDomain(res.url)}
                             </a>
                         </TableCell>
-                        <TableCell className="text-sm">
+                        <TableCell>
                             <a href={`${res.url}#${res.chunk_tag_id}`} className="link-underline-animation">
                                 {res.content.slice(0, 100) + "..."}
-
                             </a>
                         </TableCell>
                         <TableCell className="text-right">{res.prob.toFixed(2)}</TableCell>
