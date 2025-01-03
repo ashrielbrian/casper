cfbd50f04004119d44c113baa3a3488ceb0b2b1d
Brian Tang <ashrielbrian@gmail.com>
2025-01-03 23:18:50 +0800

    Fixes an issue where the promise isn't being awaited when deploying the prod build version

--- background.ts
+++ background.ts
@@ -40,7 +40,6 @@ const runInference = async (pipeline, chunk: string) => {
     });
 
     const embedding = Array.from(output.data) as number[];
-
     return embedding;
 }
 
@@ -48,67 +47,58 @@ const deleteIfUrlIsExpired = async (worker: PGliteWorker) => {
     return await deletePagesOlderThan(worker);
 }
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    // const pg = await getDB();
-    // const pg = getWorker();
-
-    console.log(`Message is of type: ${message.type}`);
-
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
     // check the URL - if it exists, check if age of the URL. if older than a day, delete the old entries, chunk new and get new embs
     if (message.type === "process_page") {
 
         const { type, url, textChunks }: { type: string, url: string, textChunks: Chunk[] } = message;
 
-        const urlIsPresent = await lockAndRunPglite(urlIsPresentOrInDatetimeRange, { url });
+        (async () => {
+            const urlIsPresent = await lockAndRunPglite(urlIsPresentOrInDatetimeRange, { url });
 
-        console.log("got back return from lock return value:", urlIsPresent)
+            if (urlIsPresent) {
+                // URL exists OR is still valid. Do not process, and return.
+                sendResponse({ ok: true, error: null, msg: `URL ${url} has been processed before. Skipping...` })
+            } else {
+                const urlId = await lockAndRunPglite(getUrlId, { url });
+                const pipeline = await getLLMPipeline();
 
-        if (urlIsPresent) {
-            // URL exists OR is still valid. Do not process, and return.
-            sendResponse({ ok: true, error: null, msg: `URL ${url} has been processed before. Skipping...` })
-        } else {
-            const urlId = await lockAndRunPglite(getUrlId, { url });
-            console.log("got back return from lock return value:", urlId)
-            const pipeline = await getLLMPipeline();
-
-            console.log(`Background received type '${type}', at ${url} with ${textChunks.length} chunks.`);
-
-            if (textChunks.length <= 0) {
-                sendResponse({ ok: false, error: null, msg: `No chunks sent.` })
-                return;
-            }
-
-            console.log(`Here is a sample chunks: ${textChunks[0]}`);
-
-            navigator.locks.request("pglite", async (lock) => {
-                let pg = await getDB()
-                for (let chunk of textChunks) {
-
-                    let embedding = await runInference(pipeline, chunk.content);
-                    console.log("Generated embedding: ", embedding.length)
-
-                    await storeEmbeddings(pg, urlId, chunk, embedding);
+                if (textChunks.length <= 0) {
+                    sendResponse({ ok: false, error: null, msg: `No chunks sent.` })
+                    return;
                 }
-            })
 
-            sendResponse({ ok: true, error: null, msg: `Done processing ${textChunks.length} chunks.` })
-        }
+                navigator.locks.request("pglite", async (lock) => {
+                    let pg = await getDB()
+                    for (let chunk of textChunks) {
+
+                        let embedding = await runInference(pipeline, chunk.content);
+                        await storeEmbeddings(pg, urlId, chunk, embedding);
+                    }
+                    sendResponse({ ok: true, error: null, msg: `Done processing ${textChunks.length} chunks.` })
+                })
+            }
+        })()
+
 
     } else if (message.type === "get_embedding") {
-        const { chunk } = message;
-        const pipeline = await getLLMPipeline();
 
-        let embedding = await runInference(pipeline, chunk);
-        console.log("Generated embedding: ", embedding.length)
+        (async () => {
+            const { chunk } = message;
+            const pipeline = await getLLMPipeline();
+            let embedding = await runInference(pipeline, chunk);
 
-        sendResponse({ ok: true, error: null, embedding })
+            sendResponse({ ok: true, error: null, embedding })
+        })()
     } else if (message.type === "clean_up") {
-        await lockAndRunPglite(deleteIfUrlIsExpired, {});
-        sendResponse({ ok: true, error: null })
+
+        (async () => {
+            await lockAndRunPglite(deleteIfUrlIsExpired, {});
+            sendResponse({ ok: true, error: null })
+        })()
     }
 
-    // Keep the message channel open for async response
+    // IMPORTANT: Keeps the message channel open for async response
     return true;
 })
-
